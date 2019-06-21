579c39fcffe1c76a68d2838868fa3cbe3a53b030
Android Studio <devandroid@ort>
2019-06-21 11:57:48 -0300

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- app/src/main/java/com/example/healthme/ListaUsuarios.java
+++ app/src/main/java/com/example/healthme/ListaUsuarios.java
@@ -8,8 +8,13 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
@@ -30,37 +35,38 @@ public class ListaUsuarios extends AppCompatActivity {
         miAdaptador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaUsuarios);
         tareaAsincronica miTarea = new tareaAsincronica();
         miTarea.execute();
+
         Log.d("AccesoApi", "Termine la ejecucion");
     }
-    public void procesarJSONleido(InputStreamReader streamLeido) {
-        JsonReader JSONleido = new JsonReader(streamLeido);
-        try {
-            JSONleido.beginObject();
-            while (JSONleido.hasNext()) {
-                String nombreElementoActual = JSONleido.nextName();
-                if (nombreElementoActual.equals("")) {
-                    int cantidadCategoria = JSONleido.nextInt();
-                    //Log.d("LecturaJSON", "La cant de categorias es " + cantidadCategoria);
 
-                } else {
-                    JSONleido.beginArray();
-                    while (JSONleido.hasNext()) {
-                        JSONleido.beginObject();
-                        while (JSONleido.hasNext()) {
-                            nombreElementoActual = JSONleido.nextName();
-                            if (nombreElementoActual.equals("Nombre")) {
-                                String valorElementoActual = JSONleido.nextString();
-                                Log.d("LecturaJSON", "valor" + valorElementoActual);
-                                listaUsuarios.add(valorElementoActual);
-                            } else {
-                                JSONleido.skipValue();
-                            }
-                        }
-                        JSONleido.endObject();
-                    }
-                    JSONleido.endArray();
-                }
+    public static String jsonToString(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public void procesarJSONleido(InputStreamReader streamLeido) {
+        Log.d("LecturaJSON","eNTRO");
+        try {
+            JSONArray listaUsuarios = new JSONArray(jsonToString(streamLeido));
+
+            for(int i = 0; i < listaUsuarios.length(); i++){
+
+                JSONObject usuario = listaUsuarios.getJSONObject(i);
+
+                Log.d("A", usuario.getString("IdUsuario"));
+                Log.d("A", usuario.getString("Apellido"));
+                Log.d("A", usuario.getString("Nombre"));
+                Log.d("A", usuario.getString("NomUsuario"));
+                Log.d("A", usuario.getString("Email"));
+
+
             }
+
+
         } catch (Exception error) {
             Log.d("LecturaJSON", "hubo un error" + error.getMessage());
         }
@@ -69,12 +75,13 @@ public class ListaUsuarios extends AppCompatActivity {
 
     class tareaAsincronica extends AsyncTask<Void, Void, Void> {
 
-        String miPC="10.0.2.2";
+
+        String miPC="10.0.2.2:50197";
 
         @Override
         protected Void doInBackground(Void... voids) {
             try {
-                URL miRuta = new URL("http://"+ miPC +":50197/api/ObtenerUsuarios");
+                URL miRuta = new URL("http://"+ miPC +"/api/ObtenerUsuarios");
                 HttpURLConnection miConexion = (HttpURLConnection) miRuta.openConnection();
                 Log.d("AccesoApi", "Me Conecto");
                 if (miConexion.getResponseCode() == 200) {
@@ -83,7 +90,6 @@ public class ListaUsuarios extends AppCompatActivity {
                     InputStream cuerpoRespuesta = miConexion.getInputStream();
                     InputStreamReader lectorRespuesta = new InputStreamReader(cuerpoRespuesta, "UTF-8");
                     procesarJSONleido(lectorRespuesta);
-
                 } else {
 
                     Log.d("AccesoApi", "error");
@@ -103,6 +109,7 @@ public class ListaUsuarios extends AppCompatActivity {
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
             miListaDeUsuarios.setAdapter(miAdaptador);
+
         }
 
     }
